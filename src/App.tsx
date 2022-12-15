c383a82c292200db2f8a98813dec2c65bda1065e
Jerzy Lozenko <112312492+IuriiLozenko@users.noreply.github.com>
2022-12-15 21:28:06 +0100

    Hooksy React

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from "react";
 import OurCustomHeader from "./Components/Header/Header";
 import Article from './Components/Article/Article';
 import Display from './Components/Display/Display';
 import Manipulator from './Components/Manipulator/Manipulator';
+
+export type count = string | number | boolean;
+
 function App() {
+
+  // pierwszy element useState: zmienna stanowa, pojemnik na dane, zmiana jej wartości powoduje przładowanie się komponentu w którym została zainicjalizowana
+  // drugi element useState: funckja aktualizująca zmienna stanową, zmienna stanowa i jej funkcja są nie rozłączne
+
+  // const [count, setCount] = useState<count>(0);
+  //const = 5 - ŹLE
+  //setCount(5) //- DOBRZE
+
+
 // sumulacja strzału do API
 const response = 15;
 const randomNumber = Math.random();
-const stringWithRandomNumberInIt = `lalalalaala ${randomNumber} eibvwuefhweiufhwe ${response} bdiobdrb`;
+const stringWithRandomNumberInIt = `lalalalala ${randomNumber} lalalalala ${response} lalalalala`;
   return (
     <div className="App">
       <OurCustomHeader />
@@ -35,3 +47,4 @@ export default App;
 // 1. Wpisuje propsa w odpowiedni komponent (nazwa propsa dowolna, wartość to zmienna ktorą chcemy przekazać)
 // 2. Odbieram obiekt z propsami w parametrze komponentu do którego te propsy przekazałem.
 // 3. Propsy ZAWSZE przychodzą w obiekcie, a sam obiekt jest parametrem. Z zasad TS wynika że parametry funkcji trzeba otypować. Do typowania obiektów używamy interfejsów. Tworzę interface dla moich propsów, będzie on nazywany według wzoru *nazwa komponentu* + Props. W samym interfejsie wypisuje wszystkie po kolei, jednocześnie podając typy ich wartości.
+
